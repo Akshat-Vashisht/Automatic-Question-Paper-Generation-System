e4d0992642ac5ef0ac7939b911d7036677e8e02b
Akshat Vashisht <akashat2003@gmail.com>
2023-03-12 23:42:54 +0530

    Updated the basic skeleton
    Added the option to upload the excel file, handled the excel file routing, reading the file and filtering the file
    
    To-do 
    Subsetting based on user input instead of hard coded values

--- Automatic Question Paper Generation System/Website/views.py
+++ Automatic Question Paper Generation System/Website/views.py
@@ -3,6 +3,9 @@ from flask_login import login_required, current_user
 from flask import Flask, request, render_template
 from werkzeug.utils import secure_filename
 import os
+import pandas as pd
+import random
+
 
 views = Blueprint("views", __name__)
 
@@ -17,3 +20,19 @@ def home():
 @login_required
 def question_paper():
     return render_template("instructions.html", user=current_user)
+
+
+@views.route("/generate", methods=["GET", "POST"])
+@login_required
+def generate_question_paper():
+    file = request.files['excel_file']
+    df = pd.read_excel(file)
+    question = df["Question"]
+    marks = df["Marks"]
+    difficulty = df["Difficulty"]
+    blooms = df["Blooms"]
+    # Test hard code
+    df = df[((df['Marks'] == 5) & (df['Difficulty'] == "Easy"))
+            | ((df['Marks'] == 4) & (df['Difficulty'] == "Medium"))]
+    df = df.sample(n=5, replace=False)
+    return render_template("generate.html", user=current_user, df=df)
